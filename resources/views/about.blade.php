68cea62a41db6cef9e40989bf7a207bae93a424e
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2025-03-15 17:32:21 +0200

    Styled the layout using Tailwind

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,8 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
+
     <h2>Hello from the about page!</h2>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
     <h2>Hello from the contact page!</h2>
 </x-layout>
