a2ac7e4db8788cf27e2eb4e76b3fb599ed222b6c
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2025-03-16 21:38:57 +0200

    Styled the nav links based on whether they are active

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,20 +17,22 @@
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
                         <div class="shrink-0">
-                            <img class="size-8"
-                                src="images/bg-flag.png"
-                                alt="Your Company">
+                            <img class="size-8" src="images/bg-flag.png" alt="Your Company">
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/"
-                                    class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                    aria-current="page">Home</a>
-                                <a href="/about"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <x-nav-link href="/" :active="request()->is('/')">
+                                    Home
+                                </x-nav-link>
+
+                                <x-nav-link href="/about" :active="request()->is('about')">
+                                    About
+                                </x-nav-link>
+
+                                <x-nav-link href="/contact" :active="request()->is('contact')">
+                                    Contact
+                                </x-nav-link>
 
                             </div>
                         </div>
@@ -56,8 +58,7 @@
                                         id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                         <span class="absolute -inset-1.5"></span>
                                         <span class="sr-only">Open user menu</span>
-                                        <img class="size-8 rounded-full bg-white-300"
-                                            src="images/userIcon.svg"
+                                        <img class="size-8 rounded-full bg-white-300" src="images/userIcon.svg"
                                             alt="">
                                     </button>
                                 </div>
@@ -94,36 +95,35 @@
             <div class="md:hidden" id="mobile-menu">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                        aria-current="page">Home</a>
-                    <a href="/about"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                    <a href="/contact"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-
+                    <x-nav-link href="/" :active="request()->is('/')">
+                        Home
+                    </x-nav-link>
+                    <x-nav-link href="/about" :active="request()->is('about')">
+                        About
+                    </x-nav-link>
+                    <x-nav-link href="/contact" :active="request()->is('contact')">
+                        Contact
+                    </x-nav-link>
                 </div>
                 <div class="border-t border-gray-700 pt-4 pb-3">
                     <div class="flex items-center px-5">
                         <div class="shrink-0">
-                            <img class="size-10 rounded-full"
-                                src="{{asset('images/userIcon.svg')}}" alt="">
+                            <img class="size-10 rounded-full" src="{{ asset('images/userIcon.svg') }}" alt="">
                         </div>
                         <div class="ml-3">
-                            <div class="text-base/5 font-medium text-white">Stefan Chervenkov</div>
-
+                            <div class="text-base font-medium text-white">Stefan Chervenkov</div>
                         </div>
                         <button type="button"
                             class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                             <span class="absolute -inset-1.5"></span>
                             <span class="sr-only">View notifications</span>
-                            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" aria-hidden="true" data-slot="icon">
+                            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                aria-hidden="true" data-slot="icon">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                             </svg>
                         </button>
                     </div>
-
                 </div>
             </div>
         </nav>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,11 @@
-<a href="{{ $href }}" class="nav-link"> 
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium'
+            : 'text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
